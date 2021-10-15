e11a3869c00be5e60981774eaea42709f8dcacbc
tarwat-uddin <t.uddin9121@gmail.com>
2021-10-15 15:34:51 +0100

    Refractor app.js

--- app.js
+++ app.js
@@ -3,19 +3,21 @@
 // Selecting elements
 const sketchpad = document.querySelector("#sketchpad");
 // Buttons
-const clearBtn = document.querySelector("#btn--clear");
 // --- BRUSH MODES
 const modeBtns = document
   .querySelector("#btn--modes")
   .getElementsByClassName("btn");
 const [normalBtn, colorBtn, shadeBtn, eraserBtn] = modeBtns;
-// Inputs --- SETTINGS
+const clearBtn = document.querySelector("#btn--clear");
+// Inputs
+// --- SETTINGS
 const canvasPixelsIpt = document.querySelector("#canvas--pixels");
 const canvasColorIpt = document.querySelector("#canvas--color");
 const brushColorIpt = document.querySelector("#brush--color");
 
 // Initial variables
 let currentMode;
+
 const canvas = {
   pixels: 16,
   color: "#f00",
@@ -25,128 +27,99 @@ const brush = {
   color: "rgb(0,0,0)",
 };
 
+(() => init())();
+
 function init() {
-  currentMode = normalBtn;
-  currentMode.classList.add("btn--active");
-  updateCanvas();
+  buildSketchpad();
+  updateBrushMode();
 }
 
-function createSquare() {
-  // Creates a new div element with square attributes
-  const div = document.createElement("div");
-  div.className = "square";
-  div.style.backgroundColor = canvas.color;
-  return div;
+// Sketchpad functions
+function resetSketchpad() {
+  destroySketchpad();
+  buildSketchpad();
+  updateSketchpad();
 }
 
-function updateSketchpad() {
-  for (let i = 0; i < canvas.pixels ** 2; i++) {
-    const node = createSquare();
-    sketchpad.appendChild(node);
-    sketchpad.style.gridTemplateRows = `repeat(${canvas.pixels}, 1fr`;
-    sketchpad.style.gridTemplateColumns = `repeat(${canvas.pixels}, 1fr)`;
-  }
-}
-
-function clearSketchpad() {
+function destroySketchpad() {
   // Destroys canvas child nodes
   while (sketchpad.firstChild) {
     sketchpad.removeChild(sketchpad.firstChild);
   }
 }
 
-function resetSketchpad() {
-  clearSketchpad();
-  updateSketchpad();
+function buildSketchpad() {
+  for (let i = 0; i < canvas.pixels ** 2; i++) {
+    const div = document.createElement("div");
+    div.className = "square";
+    div.style.backgroundColor = canvas.color;
+    sketchpad.appendChild(div);
+    sketchpad.style.gridTemplateRows = `repeat(${canvas.pixels}, 1fr`;
+    sketchpad.style.gridTemplateColumns = `repeat(${canvas.pixels}, 1fr)`;
+  }
 }
 
-function setCursor(cursorName = "brush") {
-  sketchpad.style.cursor = `url('/assets/images/${cursorName}.cur'), auto`;
-}
-
-function updateBrushMode() {
+function updateSketchpad() {
+  const setBrushCursor = (cursorName = "brush") =>
+    (sketchpad.style.cursor = `url('/assets/images/${cursorName}.cur'), auto`);
   for (const square of sketchpad.getElementsByClassName("square")) {
     const setSquareColor = (color) => (square.style.backgroundColor = color);
     let percent = 100; // Used for brush.mode shade
     square.addEventListener("mouseover", function () {
-      setCursor();
+      setBrushCursor();
       switch (brush.mode) {
         case "normal":
-          setSquareColor(brush.color);
-          break;
+          return setSquareColor(brush.color);
         case "color":
-          setSquareColor(randomColor());
+          const generateRGBValue = () => Math.trunc(Math.random() * 255) + 1;
+          const randomColor = `rgb(${generateRGBValue()},${generateRGBValue()},${generateRGBValue()})`;
+          setSquareColor(randomColor);
           break;
         case "shade":
-          const shadeColor = darkerShade(percent);
+          // Starts from white and shades until black
+          const rgbValue = (percent / 100) * 255;
+          const shadeColor = `rgb(${rgbValue},${rgbValue},${rgbValue})`;
           setSquareColor(shadeColor);
           percent -= 10;
           break;
         case "eraser":
           setSquareColor(canvas.color);
-          setCursor("eraser");
+          setBrushCursor("eraser");
           break;
       }
     });
   }
 }
 
-function updateCanvas() {
+// Brush functions
+function updateBrushMode(mode = normalBtn) {
+  if (mode.value !== brush.mode) currentMode.classList.remove("btn--active");
+  brush.mode = mode.value;
+  currentMode = mode;
+  currentMode.classList.add("btn--active");
   updateSketchpad();
-  updateBrushMode();
-}
-
-// Color functions
-function randomColor() {
-  const generateRGBValue = () => Math.trunc(Math.random() * 255) + 1;
-  return `rgb(${generateRGBValue()},${generateRGBValue()},${generateRGBValue()})`;
-}
-function darkerShade(percent) {
-  // Starts from white and shades until black
-  const rgbValue = (percent / 100) * 255;
-  return `rgb(${rgbValue},${rgbValue},${rgbValue})`;
 }
 
 // Button functionalities
-clearBtn.addEventListener("click", function () {
-  currentMode.classList.remove("btn--active");
-  resetSketchpad();
-});
 for (const mode of modeBtns) {
   mode.addEventListener("click", function () {
-    if (mode.value !== brush.mode) currentMode.classList.remove("btn--active");
-    brush.mode = mode.value;
-    currentMode = mode;
-    currentMode.classList.add("btn--active");
-    updateCanvas();
+    updateBrushMode(mode);
   });
 }
+
+clearBtn.addEventListener("click", resetSketchpad);
+
 // --- KEYBOARD SUPPORT
-document.addEventListener("keydown", function (e) {
-  let mode;
+document.addEventListener("keyup", function (e) {
   switch (e.key) {
     case "q":
-      brush.mode = "normal";
-      mode = normalBtn;
-      break;
+      return updateBrushMode();
     case "w":
-      brush.mode = "color";
-      mode = colorBtn;
-      break;
+      return updateBrushMode(colorBtn);
     case "e":
-      brush.mode = "shade";
-      mode = shadeBtn;
-      break;
+      return updateBrushMode(shadeBtn);
     case "d":
-      brush.mode = "eraser";
-      mode = eraserBtn;
-      break;
-  }
-  if (currentMode.value !== brush.mode) {
-    currentMode.classList.remove("btn--active");
-    currentMode = mode;
-    currentMode.classList.add("btn--active");
-    updateCanvas();
+      return updateBrushMode(eraserBtn);
   }
 });
 
@@ -161,11 +134,7 @@ canvasPixelsIpt.addEventListener("blur", function () {
 });
 canvasColorIpt.addEventListener("blur", function () {
   canvas.color = canvasColorIpt.value;
-  resetSketchpad();
 });
 brushColorIpt.addEventListener("blur", function () {
   brush.color = brushColorIpt.value;
 });
-
-// Main code execution
-init();
